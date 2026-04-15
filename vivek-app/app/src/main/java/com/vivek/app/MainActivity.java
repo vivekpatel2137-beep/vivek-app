64fb24e905776e62490886dfe6b37c7a7697f1a0
vivekpatel2137-beep <vivekpatel2137@gmail.com>
2026-04-15 18:37:33 +0530

    final fix build

--- vivek-app/app/src/main/java/com/vivek/app/MainActivity.java
+++ vivek-app/app/src/main/java/com/vivek/app/MainActivity.java
@@ -3,14 +3,18 @@ package com.vivek.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         WebView webview = new WebView(this);
-        webview.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+
         webview.loadUrl("file:///android_asset/index.html");
 
         setContentView(webview);
